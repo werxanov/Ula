3b10faf66d4e8236eeb4df5f19c3d47debefa14b
Ula <werxanov@gmail.com>
2021-01-13 18:02:39 +0500

    urok 14 done

--- 012 script.js
+++ 012 script.js
@@ -34,10 +34,7 @@ const personalMovieDB = {
 };
 
 // for (let i = 0; i < 2; i++) {
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', ''),
-          c = prompt('Один из последних просмотренных фильмов?', ''),
-          d = prompt('На сколько оцените его?', '');
+
 
     // if (a !=null && b!=null && a!='' && b!='' && a.length<50) {
     //     personalMovieDB.movies[a]= b;
@@ -46,10 +43,31 @@ const a = prompt('Один из последних просмотренных ф
     //     console.log('error');
     //     i--;
     // }
-personalMovieDB[a] = b;
-personalMovieDB[c] = d;
 
 
+for (let i = 0; i <2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+          
+} 
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Oshibka");
+}
 
     console.log(personalMovieDB);
 
